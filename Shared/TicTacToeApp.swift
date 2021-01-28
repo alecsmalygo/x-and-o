1ea3ee7891d9285c40ff9043d5b7cfb574ca1d5c
Alec Smalygo <ninjaalec12@gmail.com>
2021-01-28 09:42:44 -0600

    Initial Commit

---
+++ Shared/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  Shared
+//
+//  Created by This Is Alec's Account on 1/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
